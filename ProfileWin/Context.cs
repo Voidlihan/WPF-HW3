0e79c5ac5ac8a317abe896023de4048f464a018c
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-11-25 17:01:29 +0600

    Добавьте файлы проекта.

---
+++ ProfileWin/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProfileWin
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-82L5RVF8;Database=ProfileDb;Trusted_Connection=true;");
+        }
+    }
+}
